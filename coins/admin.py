1a937aadd4c40a6091c58a2bbd02bddc8485d5a8
marceloocampogit <marceloocampo1993@icloud.com>
2023-03-10 09:52:14 -0300

    Avance clase 2 y 3 v2

--- coins/admin.py
+++ coins/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
+from coins.models import Card
+
 # Register your models here.
+@admin.register(Card)
+class CardAdmin(admin.ModelAdmin):
+    list_display = ('card_name', 'card_holder', 'card_number', 'bank_name', 'valid_date', 'color')
--- coins/models.py
+++ coins/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Card(models.Model):
+
+    CHOICES = (
+        ('purple', 'purple'),
+        ('blue', 'blue'),
+        ('green', 'green'),
+        ('orange', 'orange'),
+    )
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name= 'cards')
+    card_name = models.CharField(max_length=40)
+    card_holder = models.CharField(max_length=60)
+    card_number = models.IntegerField(validators=[
+                                                MinValueValidator(1000000000000000),
+                                                MaxValueValidator(9999999999999999)
+                                                ])
+    bank_name = models.CharField(max_length=80)
+    valid_date = models.DateField()
+    color = models.CharField(max_length= 6, choices= CHOICES, default= 'purple')
+
+    def __str__(self) -> str:
+        return self.card_name
